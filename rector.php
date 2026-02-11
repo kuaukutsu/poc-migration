09b5c4c57dc554d1b225474fd83b58f687d50864
Dmitriy Krivopalov <dimarik82@gmail.com>
2026-02-11 21:07:50 +0300

    correction metadata project

--- rector.php
+++ rector.php
@@ -1,34 +1,26 @@
 <?php
 
+/**
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths(
+return Rector\Config\RectorConfig::configure()
+    ->withPaths(
         [
             __DIR__ . '/src',
             __DIR__ . '/example',
             __DIR__ . '/tests',
         ]
-    );
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->ruleWithConfiguration(
-        AddOverrideAttributeToOverriddenMethodsRector::class,
+    )
+    ->withParallel()
+    ->withCache('/tmp/var/rector')
+    ->withPhpSets()
+    ->withRules(
         [
-            'allow_override_empty_method' => true,
+            InlineConstructorDefaultToPropertyRector::class,
         ]
     );
-
-    // define sets of rules
-    $rectorConfig->sets([
-        SetList::PHP_81,
-        SetList::PHP_82,
-        SetList::PHP_83,
-    ]);
-};
